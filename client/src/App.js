8a0ad70a791cc85b74495c7a6d172ca30df004ad
wasim919 <wasimwik919@gmail.com>
2020-08-10 00:20:39 +0530

    Second commit

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 const App = () => (
   <div className='container mt-4'>
     <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
+      <i className='fab fa-react' />
+      File Upload
     </h4>
 
     <FileUpload />
